963e147544fe761e5b61d9ffce266ce153517734
Shahana Mahjubin Supty <mahsupty06@gmail.com>
2021-03-04 19:17:57 +0600

    todays last update

--- app.js
+++ app.js
@@ -23,40 +23,6 @@ app.get('/', (req, res) => {
 });
   
 
-// app.get('/posts' , (req,res) => {
-//     res.send('my server posts');
-// });
-// app.get("/", async (req, res) => {
-//     let message = "hello world!";
-  
-//     message = message.toUpperCase();
-  
-//     res.status(200).json({ message });
-//   });
-//   app.get('/', (req, res) => {
-//     res.status(200).json({
-//       error: false,
-//       message: 'Bonjour, mon ami',
-//     });
-//   });
-// router.post('/posts', async (req, res) => {
-//     const post = new Post({
-//         title: req.body.title,
-//         description: req.body.description
-//     });
-//     try{
-//     const savedPost = await post.save();
-//     res.json(savedPost);
-//     }
-//     catch (err) {
-//         res.json({message: err});
-//     }
-// })
-
-
-
-
-
 // db connection
 mongoose.connect(process.env.DB_CONNECTION,
      { useNewUrlParser: true },
--- tests/test.js
+++ tests/test.js
@@ -1,20 +1,9 @@
-const { json } = require("body-parser");
+// const { json } = require("body-parser");
 const express = require("express");
-const supertest = require("supertest");
+const request = require("supertest");
 const app = require('../app');
 
-// describe("/", () => {
-//  test("/", async () => {    
 
-//     await request(app).post("/").send(data() {
-//         return {
-//             expect(response).toBe(200)
-//         }
-//     },);
-  
-//     // expect(response.body.message).toBe("HELLO WORLD");
-//  })
-// })
 describe('GET /', () => {
   test('Responds with 200 status code and a message', async () => {
     const response = await request(app).get('/');
